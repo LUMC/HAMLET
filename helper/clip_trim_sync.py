72a4ffca0cecf93d3950dc2acdf42723dfaf4761
bow <bow@bow.web.id>
2017-04-19 17:03:46 +0200

    Update up to trimming

--- helper/clip_trim_sync.py
+++ helper/clip_trim_sync.py
@@ -3,14 +3,125 @@
 """Snakemake helper for clipping, trimming, and syncing paired-end FASTQ files."""
 
 import argparse
+import concurrent.futures
 import os
 import shutil
+import subprocess
+import sys
+
+from crimson import fastqc
+
+
+# FastQC-sickle encoding name map
+# from FastQC source
+# (uk.ac.babraham.FastQC.Sequence.QualityEncoding.PhredEncoding.java)
+# and sickle source (src/sickle.h)
+FQ_SICKLE_ENC_MAP = {
+    "Sanger / Illumina 1.9": ("sanger", 33),
+      "Illumina <1.3": ("solexa", 59),
+    "Illumina 1.3": ("illumina", 64),
+    "Illumina 1.5": ("illumina", 64),
+}
+
+
+def parse_contam_file(contam_file, delimiter="\t"):
+    """Returns a dictionary of contaminant sequences names and their
+    sequences in the given contaminants file."""
+    with open(contam_file, "r") as source:
+        # read only lines not beginning with "#" and discard empty lines
+        lines = filter(None, (line.strip() for line in source if not
+            line.startswith("#")))
+        # parse contam seq lines into lists of [id, sequence]
+        parse = lambda line: filter(None, line.split(delimiter))
+        parsed = (parse(line) for line in lines)
+        # and create a dictionary, key=sequence id and value=sequence
+        contam_ref = {name: seq for name, seq in parsed}
+
+    return contam_ref
+
+
+def get_contams_present(fqcd, contamd):
+    """Returns the full sequence of found FastQC contaminants."""
+    contam_names = {x["Possible Source"]
+                    for x in fqcd["Overrepresented sequences"]["contents"]}
+    in_sample = lambda rid: any([cid.startswith(rid)
+                                 for cid in contam_names if cid != "No Hit"])
+    contams_present = {x: y for x, y in contamd.items() if in_sample(x)}
+    return contams_present
+
+
+def construct_command(in_fname, out_fname, enc, enc_offset, adapters):
+    fifos = []
+    if adapters:
+        cutadapt_fifop = out_fname + ".cutadapt.fifo"
+        cutadapt_fifo = os.mkfifo(cutadapt_fifop)
+        fifos.append(cutadapt_fifop)
+        cutadapt_stderr = out_fname + ".cutadapt"
+
+        cutadapt_toks = ["adapters"]
+        for adapter in adapters:
+            cutadapt_toks.extend(["-a", adapter])
+        cutadapt_toks.append(in_fname)
+
+        cutadapt_proc = subprocess.Popen(
+            cutadapt_toks, stdout=cutadapt_fifo, stderr=open(cutadapt_stderr, "w"))
+
+        in_fname = cutadapt_fifop
+
+    sickle_fifop = out_fname + ".sickle.fifop"
+    sickle_fifo = os.mkfifo(sickle_fifop)
+    sickle_stdout = out_fname + ".sickle"
+    fifos.append(sickle_fifop)
+
+    sickle_proc = subprocess.Popen(
+        ["sickle", "se", "-f", in_fname, "-o", sickle_fifop, "-t", "sanger"],
+        stdout=open(sickle_stdout, "w"))
+
+    gzip_proc = subprocess.Popen(
+        ["gzip", "-c"], stdout=open(out_fname, "w"), stdin=open(sickle_fifop, "r"))
+
+    return gzip_proc, fifos
+
+
+def process_read(in_fname, in_fqc_dir, out_fname, contams_fname):
+    fqcd = fastqc.parse(in_fqc_dir)
+    contamd = parse_contam_file(contams_fname)
+
+    raw_enc = fqcd["Basic Statistics"]["contents"]["Encoding"]
+    enc, enc_offset = FQ_SICKLE_ENC_MAP[raw_enc]
+    adapters = get_contams_present(fqcd, contamd)
+
+    cmd, fifos = construct_command(in_fname, out_fname, enc, enc_offset, adapters)
+
+    try:
+        cmd.wait()
+    except:
+        raise
+    finally:
+        for fifo in fifos:
+            os.unlink(fifo)
 
 
 def clip_sync_trim(input_r1, input_r2, input_fqc_r1, input_fqc_r2,
                    output_r1, output_r2, contams_fname):
-    shutil.copy(input_r1, output_r1)
-    shutil.copy(input_r2, output_r2)
+
+    with concurrent.futures.ProcessPoolExecutor(max_workers=2) as executor:
+
+        futures = {executor.submit(process_read, i, j, o, contams_fname): (i, j, o)
+                   for i, j, o in ((input_r1, input_fqc_r1, output_r1),
+                                   (input_r2, input_fqc_r2, output_r2))}
+
+        for future in concurrent.futures.as_completed(futures):
+            in_fname, _, out_fname = futures[future]
+            try:
+                future.result()
+            except Exception as exc:
+                print("processing of {!r} generated an exception: {}"
+                      .format(in_fname, exc), file=sys.stderr)
+                raise
+            else:
+                print("processed {!r} to {!r}".format(in_fname, out_fname),
+                      file=sys.stderr)
 
 
 if __name__ == "__main__":
@@ -21,9 +132,9 @@ if __name__ == "__main__":
     parser.add_argument("--i2", type=str, required=True,
                         help="path to input R2 FASTQ file")
     parser.add_argument("--fqc1", type=str, required=True,
-                        help="path to input FastQC JSON output from R1")
+                        help="path to FastQC R1 output directory")
     parser.add_argument("--fqc2", type=str, required=True,
-                        help="path to input FastQC JSON output from R2")
+                        help="path to FastQC R2 output directory")
     parser.add_argument("--o1", type=str, required=True,
                         help="path to output R1 FASTQ file")
     parser.add_argument("--o2", type=str, required=True,
