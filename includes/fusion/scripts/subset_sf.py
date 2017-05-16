f608a74a95d0fa82576fcec7b074ea9ec4dfe508
bow <bow@bow.web.id>
2017-05-16 11:52:27 +0200

    Add and update rules for combining fusion plots

---
+++ includes/fusion/scripts/subset_sf.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+
+import sys
+
+
+if __name__ == "__main__":
+
+    fuma_fname = sys.argv[1]
+    sf_fname = sys.argv[2]
+
+    isect = set([])
+    with open(fuma_fname, "r") as src:
+        src.readline()
+        for line in (raw_line.strip() for raw_line in src):
+            cols = filter(lambda x: len(x) > 0, line.split("\t"))
+            if len(cols) < 5:
+                continue
+            lefts = cols[0].split(":")
+            rights = cols[1].split(":")
+            for l in lefts:
+                for r in rights:
+                    isect.add((l, r))
+
+    with open(sf_fname, "r") as src:
+        print(src.readline().strip())
+        for line in (raw_line.strip() for raw_line in src):
+            cols = line.split("\t")
+            fusion = (cols[4].split("^")[0], cols[6].split("^")[0])
+            if fusion in isect:
+                print(line)
