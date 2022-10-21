1df2d1d0eb71652cf8b51142b458ceaeb44a4a1c
Redmar van den Berg <RedmarvandenBerg@lumc.nl>
2022-10-21 14:59:20 +0200

    Pass appropriate files directly
    Instead of globbing around in the fusing folder and pulling out stuff we
    need.

--- includes/fusion/scripts/json-output.py
+++ includes/fusion/scripts/json-output.py
@@ -1,70 +1,55 @@
 #!/usr/bin/env python
 
 import json
-import sys
 import argparse
 
-from pathlib import Path
+def parse_top20(tp):
+    with open(tp, "r") as src:
+        sft = []
+        for lineno, line in enumerate(src):
+            if lineno == 0:
+                continue
+            name, jr_count, sf_count, fusion_type, *_ = line.split("\t")
+            sft.append({"name": name, "jr_count": int(jr_count),
+                        "sf_count": int(sf_count), "type": fusion_type,})
+            if lineno > 20:
+                break
+    return sft
 
-def fusion_results(fusion_results_dir, intersected):
-    frd = Path(fusion_results_dir)
-    sf_plot = next(frd.rglob("*star-fusion-circos/*.png"))
-    sf_table = next(frd.glob("*.star-fusion"))
-    rv = {
-        "intersected": intersected,
-        "plots": {
-            "star-fusion": str(sf_plot.resolve())
-        },
-        "tables": {
-            "star-fusion": {"path": str(sf_table.resolve())},
-        }
+def fusion_results(args):
+    # Initialise the results dictionary
+    results = {
+            "intersected": args.intersected,
+            "plots": dict(),
+            "tables": dict()
     }
 
-    def parse_top20(tp):
-        with open(tp, "r") as src:
-            sft = []
-            for lineno, line in enumerate(src):
-                if lineno == 0:
-                    continue
-                name, jr_count, sf_count, fusion_type, *_ = line.split("\t")
-                sft.append({"name": name, "jr_count": int(jr_count),
-                            "sf_count": int(sf_count), "type": fusion_type,})
-                if lineno > 20:
-                    break
-        return sft
+    # Add the results for each tool
+    for tool, plot, table in zip(args.tools, args.plots, args.tables):
+        results["plots"][tool] = plot
+        results["tables"][tool] = {
+            "path": table,
+            "top20": parse_top20(table)
+        }
 
-    rv["tables"]["star-fusion"]["top20"] = \
-        parse_top20(rv["tables"]["star-fusion"]["path"])
-
-    if intersected:
-        fc_plot = next(frd.rglob("*fusioncatcher-circos/*.png"))
-        fc_table = next(frd.glob("*.star-fusion"))
-        rv["plots"]["fusioncatcher"] = str(fc_plot.resolve())
-        rv["tables"]["fusioncatcher"] = {"path": str(fc_table.resolve())}
-        rv["tables"]["fusioncatcher"]["top20"] = \
-            parse_top20(rv["tables"]["fusioncatcher"]["path"])
-
-        isect_plot = next(frd.rglob("*sf-isect-circos/*.png"))
-        isect_table = next(frd.glob("*.sf-isect"))
-        rv["plots"]["intersection"] = str(isect_plot.resolve())
-        rv["tables"]["intersection"] = {"path": str(isect_table.resolve())}
-        rv["tables"]["intersection"]["top20"] = \
-            parse_top20(rv["tables"]["intersection"]["path"])
-
-    return rv
+    return results
 
 
 def main(args):
     """ Create json output of fusion results """
-    results = fusion_results(args.fusion_results_dir, args.intersected)
-    json.dump({"fusion": results}, sys.stdout, sort_keys=True, indent=2)
+    results = fusion_results(args)
+    print(json.dumps({"fusion": results}, sort_keys=True, indent=2))
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument('--intersected', default=False, action='store_true')
-    parser.add_argument('fusion_results_dir')
+    parser.add_argument('--tools', required=True, nargs='+', help='Tools')
+    parser.add_argument('--plots', required=True, nargs='+', help='PNG plots, in the same order as --tools')
+    parser.add_argument('--tables', required=True, nargs='+', help='Result tables, in the same order as --tools')
 
     args = parser.parse_args()
+    assert len(args.tools) == len(args.plots)
+    assert len(args.tools) == len(args.tables)
     main(args)
 
