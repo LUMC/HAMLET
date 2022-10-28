30d541daa5f0d40ac5ab5269d70c11580eace738
Redmar van den Berg <RedmarvandenBerg@lumc.nl>
2022-10-28 08:52:44 +0200

    Remove click, simplify code

--- scripts/create_summary.py
+++ scripts/create_summary.py
@@ -1,152 +1,7 @@
 #!/usr/bin/env python
 
+import argparse
 import json
-import csv
-from pathlib import Path
-from collections import defaultdict
-
-import click
-from crimson import picard, vep
-
-
-def process_aln_stats(path):
-    pd = picard.parse(path)
-    raw = next(x for x in pd["metrics"]["contents"]
-               if x["CATEGORY"] == "PAIR")
-
-    aln_proper_pairs = (raw["READS_ALIGNED_IN_PAIRS"] -
-                        raw["PF_READS_IMPROPER_PAIRS"])
-
-    return {
-        "num_aligned_bases": raw["PF_ALIGNED_BASES"],
-        "num_aligned_reads": raw["PF_READS_ALIGNED"],
-        "num_aligned_reads_proper_pairs": aln_proper_pairs,
-        "num_total_reads": raw["TOTAL_READS"],
-        "pct_adapter": raw["PCT_ADAPTER"],
-        "pct_aligned_reads_from_total": (raw["PF_READS_ALIGNED"] * 100. /
-                                         raw["TOTAL_READS"]),
-        "pct_aligned_reads_proper_pairs": (aln_proper_pairs * 100. /
-                                           raw["PF_READS_ALIGNED"]),
-        "pct_chimeras": raw["PCT_CHIMERAS"],
-        "rate_indel": raw["PF_INDEL_RATE"],
-        "rate_mismatch": raw["PF_MISMATCH_RATE"],
-        "strand_balance": raw["STRAND_BALANCE"],
-    }
-
-
-def process_rna_stats(path):
-    pd = picard.parse(path)
-    raw = pd["metrics"]["contents"]
-    cov_sort_key = lambda x: x["normalized_position"]
-    return {
-        "median_3prime_bias": raw["MEDIAN_3PRIME_BIAS"],
-        "median_5prime_bias": raw["MEDIAN_5PRIME_BIAS"],
-        "median_5prime_to_3prime_bias": raw["MEDIAN_5PRIME_TO_3PRIME_BIAS"],
-        "median_cv_coverage": raw["MEDIAN_CV_COVERAGE"],
-        "num_coding_bases": raw["CODING_BASES"],
-        "num_intergenic_bases": raw["INTERGENIC_BASES"],
-        "num_intronic_bases": raw["INTRONIC_BASES"],
-        "num_mrna_bases": raw["CODING_BASES"] + raw["UTR_BASES"],
-        "num_ribosomal_bases": (raw["RIBOSOMAL_BASES"]
-                                if raw["RIBOSOMAL_BASES"] != ""
-                                else None),
-        "num_total_bases": raw["PF_BASES"],
-        "num_utr_bases": raw["UTR_BASES"],
-        "pct_coding_bases": raw["PCT_CODING_BASES"],
-        "pct_intergenic_bases": raw["PCT_INTERGENIC_BASES"],
-        "pct_intronic_bases": raw["PCT_INTRONIC_BASES"],
-        "pct_mrna_bases": raw["PCT_MRNA_BASES"],
-        "pct_ribosomal_bases": (raw["RIBOSOMAL_BASES"] * 100. / raw["PF_ALIGNED_BASES"]
-                                if raw["RIBOSOMAL_BASES"] != ""
-                                else None),
-        "pct_utr_bases": raw["PCT_UTR_BASES"],
-        "normalized_cov": [item["All_Reads.normalized_coverage"]
-                           for item in sorted(pd["histogram"]["contents"],
-                                              key=cov_sort_key, reverse=False)]
-    }
-
-
-def process_insert_stats(path):
-    pd = picard.parse(path)
-    raw = pd["metrics"]["contents"]
-    # Raw can be a list if there are more than one PAIR_ORIENTATION.
-    if isinstance(raw, list):
-        raw = next(r for r in raw if r["PAIR_ORIENTATION"] == "FR")
-    return {
-        "median_absolute_deviation": raw["MEDIAN_ABSOLUTE_DEVIATION"],
-        "median_insert_size": raw["MEDIAN_INSERT_SIZE"],
-        "min_insert_size": raw["MIN_INSERT_SIZE"],
-        "max_insert_size": raw["MAX_INSERT_SIZE"],
-    }
-
-
-def process_var_stats(path):
-    pd = vep.parse(path)
-
-    # If there are no variants, insert an empty defaultdict so that any queried
-    # value returns 0
-    if "Variants by chromosome" not in pd:
-        pd["Variants by chromosome"] = defaultdict(int)
-
-    return {
-        "coding_consequences": {k: v for k, v in pd["Coding consequences"].items()},
-        "num_deletions": pd["Variant classes"].get("deletion", 0),
-        "num_insertions": pd["Variant classes"].get("insertion", 0),
-        "num_snvs": pd["Variant classes"].get("SNV", 0),
-        "per_chromosome": {k: v
-                             for k, v in pd["Variants by chromosome"].items()
-                             if k in {str(i) for i in range(1, 23)}.union({"X", "Y", "MT"})},
-        "polyphen": {
-            "num_benign_variants": pd["PolyPhen summary"].get("benign", 0),
-            "num_possibly_damaging_variants":
-            pd["PolyPhen summary"].get("possibly damaging", 0),
-            "num_probably_damaging_variants":
-            pd["PolyPhen summary"].get("probably damaging", 0),
-            "num_unknown_variants": pd["PolyPhen summary"].get("unknown", 0),
-        },
-        "sift": {
-            "num_deleterious_variants":
-            pd["SIFT summary"].get("deleterious", 0),
-            "num_tolerated_variants":
-            pd["SIFT summary"].get("tolerated", 0),
-        },
-    }
-
-
-def process_exon_cov_stats(path, idm):
-    with open(path, "r") as src:
-        raw = json.load(src)
-
-    tid_map = {item["gene_symbol"]: set(item["transcript_ids"])
-               for item in idm}
-
-    tempd = {}
-    for val in raw.values():
-        gene = val.pop("gx")
-        if gene not in tempd:
-            tempd[gene] = {}
-        trx = val.pop("trx")
-        if trx not in tid_map[gene]:
-            continue
-        if trx not in tempd[gene]:
-            tempd[gene][trx] = {}
-        exon = int(val["exon_num"])
-        assert exon not in tempd[gene][trx], f"{gene}:{trx}:{exon}"
-        tempd[gene][trx][exon] = val
-
-    return {gid: {tid: sorted([exn for exn in tv.values()],
-                               key=lambda x: int(x["exon_num"]))
-                  for tid, tv in gv.items()}
-            for gid, gv in tempd.items()}
-
-
-def post_process(cs):
-    cs["aln"]["num_total_bases"] = (cs["rna"]
-                                          .pop("num_total_bases"))
-    pct_bases_aln = (cs["aln"]["num_aligned_bases"] * 100. /
-                     cs["aln"]["num_total_bases"])
-    cs["aln"]["pct_aligned_bases_from_total"] = pct_bases_aln
-    return cs
 
 
 def parse_idm(fh):
@@ -161,160 +16,38 @@ def parse_idm(fh):
     return idms
 
 
-def add_variant_plots(idm, var_plot_dir):
-    idms = set([])
-    for item in idm:
-        gsym = item["gene_symbol"]
-        assert gsym not in idms, item
-        idms.add(gsym)
-
-    plots = []
-    vpd = Path(var_plot_dir)
-    for png in vpd.glob("*.png"):
-        stem = png.stem
-        _, gene = png.stem.rsplit("_gene_", 1)
-        if gene in idms:
-            plots.append({"path": str(png.absolute()), "gene": gene})
-
-    return plots
-
-def add_variant_overview(idm, fn_csv):
-    idms = set([])
-    for item in idm:
-        gsym = item["gene_symbol"]
-        assert gsym not in idms, item
-        idms.add(gsym)
-
-    rv = {}
-    with open(fn_csv, "r") as src:
-        reader = csv.DictReader(src)
-        for row in reader:
-            gs = row["gene_symbol"]
-            if gs not in idms:
-                continue
-            if gs not in rv:
-                rv[gs] = []
-
-            # Fields to include in the variant overview
-            report_fields = {
-                "HGVSc", "HGVSp", "Existing_variation", "FREQ",
-                "is_in_hotspot", "PVAL"
-            }
-            # Extract relevant fields
-            d = {k: v for k, v in row.items() if k in report_fields }
-
-            # Replace NA values with an empty string
-            d = {k: v if v != "NA" else "" for k, v in d.items()}
-
-            # Update field types
-            d["FREQ"] = float(d["FREQ"][:-1]) # Cut off %
-            d["is_in_hotspot"] = d["is_in_hotspot"] == "yes"
-            d["PVAL"] = float(d["PVAL"])
-            if not d["Existing_variation"]:
-                d["Existing_variation"] = list()
-            else:
-                d["Existing_variation"] = d["Existing_variation"].split(",")
-
-            rv[gs].append(d)
-
-    return rv
-
-
-def add_expr_results(exon_ratios_path):
-    rv = []
-    with open(exon_ratios_path, "r") as src:
-        header_cols = next(src).strip().split("\t")
-        for line in (l.strip() for l in src):
-            d = dict(zip(header_cols, line.split("\t")))
-
-            # If there is no ratio, set it to zero
-            if d["ratio"] == "":
-                d["ratio"] = 0
-
-            # Update types
-            d["count"] = int(d["count"])
-            d["divisor_exp"] = int(d["divisor_exp"])
-            d["ratio" ] = float(d["ratio"])
-            d["above_threshold"] = d["above_threshold"] == "yes"
-            del d["sample_name"]
-            rv.append(d)
-    return rv
-
-
-def add_itd_table(csv_fname):
-    rv = []
-    with open(csv_fname, "r") as src:
-        header_cols = next(src).strip().split("\t")
-        for line in (l.strip() for l in src):
-            d = dict(zip(header_cols, line.split("\t")))
-
-            # Integer fields
-            int_fields = [
-                "fuzziness", "rose_end_anchor_pos", "rose_end_count",
-                "rose_end_pos", "rose_start_anchor_pos", "rose_start_count",
-                "rose_start_pos"
-            ]
-
-            # Convert values to int
-            for field in int_fields:
-                d[field] = int(d[field])
-
-            # Convert to list of ints
-            d["td_ends"] = [int(x) for x in d["td_ends"].split(",")]
-            d["td_starts"] = [int(x) for x in d["td_starts"].split(",")]
-
-            rv.append(d)
-    return rv
-
-
-@click.command(context_settings={"help_option_names": ["-h", "--help"]})
-@click.argument("id_mappings_path", type=click.File("r"))
-@click.argument("flt3_csv",
-                type=click.Path(exists=True, dir_okay=False))
-@click.argument("flt3_plot",
-                type=click.Path(exists=True, dir_okay=False))
-@click.argument("kmt2a_csv",
-                type=click.Path(exists=True, dir_okay=False))
-@click.argument("kmt2a_plot",
-                type=click.Path(exists=True, dir_okay=False))
-@click.option("-r", "--run-name", type=str,
-              help="Name of the run in which the stats were generated.")
-@click.option("-n", "--sample-name", type=str,
-              help="Name of the sample from which the stats were generated.")
-@click.option("--pipeline-version", type=str,
-              help="Version string of the pipeline.")
-@click.option("--module", type=str, multiple=True,
-              help="JSON outputs from various modules")
-def main(id_mappings_path, flt3_csv, flt3_plot, kmt2a_csv, kmt2a_plot,
-         run_name, sample_name, pipeline_version, module):
+def main(args):
     """Helper script for combining multiple stats files into one JSON."""
-    idm = parse_idm(id_mappings_path)
+    with open(args.id_mappings_path) as fin:
+        idm = parse_idm(fin)
     combined = {
         "metadata": {
-            "pipeline_version": pipeline_version,
-            "run_name": run_name,
-            "sample_name": sample_name,
+            "pipeline_version": args.pipeline_version,
+            "run_name": args.run_name,
+            "sample_name": args.sample_name,
             "genes_of_interest": idm,
         },
         "modules": dict()
     }
 
     # Read and add module json files
-    for m in module:
+    for m in args.module:
         with open(m) as fin:
             module = json.load(fin)
         for key, data in module.items():
             combined["modules"][key] = data
 
-    combined["modules"]["itd"] = {
-        "flt3": {"path": str(Path(flt3_plot).resolve()),
-                 "table": add_itd_table(flt3_csv)},
-        "kmt2a": {"path": str(Path(kmt2a_plot).resolve()),
-                  "table": add_itd_table(kmt2a_csv)},
-    }
     print(json.dumps(combined, sort_keys=True, indent=2))
 
 
 if __name__ == "__main__":
-    main()
+    parser = argparse.ArgumentParser()
 
+    parser.add_argument("id_mappings_path")
+    parser.add_argument("--run-name", help="Name of the run in which the stats were generated.")
+    parser.add_argument("--sample-name", help="Name of the sample from which the stats were generated.")
+    parser.add_argument("--pipeline-version", help="Version string of the pipeline.")
+    parser.add_argument("--module", action="append", help="JSON outputs from various modules")
+
+    args = parser.parse_args()
+    main(args)
