05e110b9eb7c52145ece486974414173c71ab636
Redmar van den Berg <RedmarvandenBerg@lumc.nl>
2022-12-01 10:12:53 +0100

    Create refflat file from GTF

---
+++ utilities/deps/scripts/rewrite-refflat.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+import sys
+
+def main():
+    refflat = sys.argv[1]
+
+    # Put the gene name in the first column
+    order = [11, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+    with open(refflat) as fin:
+        for line in fin:
+            spline = line.strip('\n').split('\t')
+            if not spline[11]:
+                print(f"Skipping {spline[0]}, no gene name", file=sys.stderr)
+                continue
+            print(*[spline[f] for f in order ], sep='\t')
+
+if __name__ == '__main__':
+    main()
