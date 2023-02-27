75028ce03796ecd007a5562461c3699dd5be98cd
Redmar van den Berg <RedmarvandenBerg@lumc.nl>
2023-02-27 15:12:18 +0100

    Cleanup and refactor

--- includes/snv-indels/scripts/filter_vep.py
+++ includes/snv-indels/scripts/filter_vep.py
@@ -3,8 +3,6 @@
 import argparse
 import json
 
-from functools import partial
-
 # From most to least severe, taken from the ensembl website
 # https://www.ensembl.org/info/genome/variation/prediction/predicted_data.html
 severity = [
@@ -57,6 +55,7 @@ class VEP(dict):
         tc = self["transcript_consequences"]
         tc = [x for x in tc if x["transcript_id"] in transcripts]
         self["transcript_consequences"] = tc
+        self.update_most_severe()
 
 
     def filter_consequence_term(self, consequences):
@@ -64,76 +63,24 @@ class VEP(dict):
         tc = self["transcript_consequences"]
         tc = [x for x in tc if not set(x["consequence_terms"]).isdisjoint(consequences)]
         self["transcript_consequences"] = tc
-
-def gene_of_interest(cons, genes):
-    """Is a VEP consequence applicable to a gene of interest"""
-    return cons["gene_id"] in genes
+        self.update_most_severe()
 
 
-def transcript_of_interest(cons, transcripts):
-    """Is a VEP consequence applicable to a transcript of interest"""
-    return cons["transcript_id"] in transcripts
+    def update_most_severe(self):
+        """The most severe consequence for all genes and transcript is stored at
+        the top level of the VEP object. After filtering the transcript
+        consequences, we need to update this field
+        """
+        # Gather all consequences
+        cons = set()
+        for consequence in self["transcript_consequences"]:
+            cons.update(consequence["consequence_terms"])
 
-
-def consequence_of_interest(cons, genes, transcripts, impact=None):
-    """Is a VEP consequence of interest for both gene and transcript"""
-    return all(
-        [
-            gene_of_interest(cons, genes),
-            transcript_of_interest(cons, transcripts),
-            # Only filter on impact if one was specified
-            cons["impact"] == impact if impact else True,
-        ]
-    )
-
-
-def consequences_of_interest(cons, genes, transcripts, impact=None):
-    """Filter consequences to only those of interest"""
-    return [
-        con for con in cons if consequence_of_interest(con, genes, transcripts, impact)
-    ]
-
-
-def update_most_severe(vep):
-    """The most severe consequence for all genes and transcript is stored at
-    the top level of the VEP object. After filtering the transcript
-    consequences, we need to update this field
-    """
-    # Gather all consequences
-    cons = set()
-    for consequence in vep["transcript_consequences"]:
-        cons.update(consequence["consequence_terms"])
-
-    # Set the first matching consequence (they are sorted based on severity)
-    for term in severity:
-        if term in cons:
-            vep["most_severe_consequence"] = term
-            break
-
-
-def vep_of_interest(vep, genes, transcripts, impact=None):
-    """Return a new VEP object which only contains consequences of interest
-
-    1. Restrict the transcript_consequences to only include genes/transcripts
-    of interest, and the impact, if specified.
-    2. Rewrite the most_severe_consequence based on the remaining transcripts.
-    """
-    # Copy the VEP object
-    new_vep = vep.copy()
-
-    # Extract the transcript consequences
-    cons = vep.get("transcript_consequences", list())
-
-    # Extract only the transcripts of interest
-    cons_int = consequences_of_interest(cons, genes, transcripts, impact)
-
-    # Replace the transcripts
-    new_vep["transcript_consequences"] = cons_int
-
-    # Set the most severe consequence
-    update_most_severe(new_vep)
-
-    return new_vep
+        # Set the first matching consequence (they are sorted based on severity)
+        for term in severity:
+            if term in cons:
+                self["most_severe_consequence"] = term
+                break
 
 
 def read_goi_file(fname):
@@ -157,13 +104,17 @@ def parse_vep_json(vep_file):
             yield json.loads(line)
 
 
-def main(vep_file, goi_file, impact):
+def main(vep_file, goi_file, consequences):
     # Get genes and transcripts of interest
     goi, toi = read_goi_file(goi_file)
 
     for variant in parse_vep_json(vep_file):
-        vep = vep_of_interest(variant, goi, toi, impact)
-        # If there is no consequence of interest
+        # Filter on transcript of interest
+        vep.filter_transcript_id(toi)
+        # Filter on consequences of interest
+        vep.filter_consequence_term(consequences)
+
+        # If there is no consequence of interest left
         if not vep["transcript_consequences"]:
             continue
         print(json.dumps(vep, sort_keys=True))
@@ -174,12 +125,23 @@ if __name__ == "__main__":
         "Extract genes (and transcript) of interest from VEP output"
     )
 
+    # Default consequences of interest
+    cons = [
+        "stop_gained",
+        "frameshift_variant",
+        "stop_lost",
+        "start_lost",
+        "inframe_insertion",
+        "inframe_deletion",
+        "protein_altering_variant",
+        "missense_variant"
+    ]
+
     parser.add_argument("vep", help="VEP json output file")
     parser.add_argument("goi", help="Genes of interest")
-    parser.add_argument(
-        "--impact", choices=["HIGH", "MODERATE", "MODIFIER"], default=None
-    )
+    parser.add_argument( "--consequences", nargs='+',
+            type=str, default=cons)
 
     args = parser.parse_args()
 
-    main(args.vep, args.goi, args.impact)
+    main(args.vep, args.goi, args.consequenes)
--- includes/snv-indels/scripts/test_filter_vep.py
+++ includes/snv-indels/scripts/test_filter_vep.py
@@ -1,37 +1,9 @@
 #!/usr/bin/env python3
 
-from filter_vep import (
-    VEP,
-    gene_of_interest,
-    consequence_of_interest,
-    consequences_of_interest,
-    vep_of_interest,
-    update_most_severe,
-)
+from filter_vep import VEP
 
 import json
 import pytest
-
-
-def make_consequence(
-    gene, transcript, consequence_terms=["stop_lost"], impact="MODIFIER"
-):
-    return {
-        "gene_id": gene,
-        "transcript_id": transcript,
-        "consequence_terms": consequence_terms,
-        "impact": impact,
-    }
-
-
-@pytest.fixture
-def gene1():
-    return make_consequence("gene1", "transcript1", impact="HIGH")
-
-
-@pytest.fixture
-def gene2():
-    return make_consequence("gene2", "transcript2", impact="HIGH")
 
 
 @pytest.fixture
@@ -39,13 +11,6 @@ def vep():
     with open("test/data/output/v2/vep.json") as fin:
         js = json.load(fin)
     return VEP(js[0])
-
-# Compare vs gene1
-GENES_OF_INTEREST = [
-        ({"gene1"}, True),
-        ({"gene2", "gene3", "gene1"}, True),
-        ({"gene9"}, False),
-]
 
 
 # transcripts, transcript_consequences lenght
@@ -84,84 +49,16 @@ def test_filter_consequence_emtpy(vep):
     assert not vep["transcript_consequences"]
 
 
-@pytest.mark.parametrize(["goi", "boolean"], GENES_OF_INTEREST)
-def test_gene_of_interest(gene1, goi, boolean):
-    """Test if gene1 is a gene of interest
-
-    Relative to genes of interest in goi
-    """
-    assert gene_of_interest(gene1, goi) == boolean
-
-
-def test_consequence_of_interest(gene1):
-    """Test recognition of consequence of interest"""
-    assert consequence_of_interest(gene1, ["gene1"], ["transcript1"])
-
-
-def test_consequences_of_interest(gene1, gene2):
-    """Test filtering only consequences of interest"""
-    consequences = [gene1, gene2]
-    genes = {"gene1"}
-    transcripts = {"transcript1"}
-    filtered = consequences_of_interest(consequences, genes, transcripts)
-    assert len(filtered) == 1
-    con = filtered[0]
-    assert con["gene_id"] == "gene1"
-    assert con["transcript_id"] == "transcript1"
-
-
-def test_consequence_of_interest_impact():
-    genes = [f"gene{i}" for i in [1, 2, 3]]
-    trans = [f"transcript{i}" for i in [1, 2, 3]]
-
-    c1 = make_consequence("gene1", "transcript1")
-    c2 = make_consequence("gene2", "transcript2", impact="HIGH")
-    c3 = make_consequence("gene3", "transcript3", impact="MODERATE")
-
-    assert consequence_of_interest(c1, genes, trans, "MODIFIER")
-    assert not consequence_of_interest(c1, genes, trans, "HIGH")
-
-    assert consequence_of_interest(c2, genes, trans, "HIGH")
-    assert not consequence_of_interest(c2, genes, trans, "MODIFIER")
-
-    # Test without an impact specified
-    assert consequence_of_interest(c3, genes, trans, "MODERATE")
-    assert consequence_of_interest(c3, genes, trans, None)
-
-
-def test_multiple_consequences_of_interest(gene1, gene2):
-    """Test returning multiple consequences of interest"""
-    consequences = [gene1, gene2]
-    genes = {"gene1", "gene2"}
-    transcripts = {"transcript1", "transcript2"}
-    filtered = consequences_of_interest(consequences, genes, transcripts)
-    assert consequences == filtered
-
-
-def test_multiple_consequences_of_interest_HIGH(vep):
-    genes = "gene1 gene2 gene3".split()
-    transcript = "transcript1 transcript2 transcript3".split()
-
-    cons = consequences_of_interest(
-        vep["transcript_consequences"], genes, transcript, "HIGH"
-    )
-    assert len(cons) == 1
-
-    con = cons[0]
-    assert con["gene_id"] == "gene2"
-
-
 def test_vep_of_interest_one_transcript(vep):
     """Test rewriting the VEP object when there is a single transcript of
     interest"""
-    genes = ["gene3"]
     transcripts = ["transcript3"]
 
-    # Restrict
-    new_vep = vep_of_interest(vep, genes, transcripts)
+    # Restrict to transcript of interest
+    vep.filter_transcript_id(transcripts)
 
     # Get the consequences after rewriting the VEP obejct
-    cons = new_vep["transcript_consequences"]
+    cons = vep["transcript_consequences"]
 
     # Test that only gene3 is left
     assert len(cons) == 1
@@ -170,19 +67,19 @@ def test_vep_of_interest_one_transcript(vep):
     cons[0]["consequence_terms"] == ["stop_gained"]
 
     # Test that we set the most severe consequence for transcript3
-    assert new_vep["most_severe_consequence"] == "stop_gained"
+    assert vep["most_severe_consequence"] == "stop_gained"
 
 
 def test_update_most_severe(vep):
-    update_most_severe(vep)
+    vep.update_most_severe()
     assert vep["most_severe_consequence"] == "transcript_ablation"
 
     # Remove gene1
     vep["transcript_consequences"].pop(0)
-    update_most_severe(vep)
+    vep.update_most_severe()
     assert vep["most_severe_consequence"] == "splice_acceptor_variant"
 
     # Remove gene2
     vep["transcript_consequences"].pop(0)
-    update_most_severe(vep)
+    vep.update_most_severe()
     assert vep["most_severe_consequence"] == "stop_gained"
