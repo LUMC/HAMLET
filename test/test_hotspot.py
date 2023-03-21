85e82bb95b3a9db5840ac41b66c3e9cb9791efb4
Redmar van den Berg <RedmarvandenBerg@lumc.nl>
2023-03-21 08:27:16 +0100

    Refactor tests for hotspots

--- test/test_hotspot.py
+++ test/test_hotspot.py
@@ -2,18 +2,25 @@ import json
 import pytest
 import pathlib
 
+HOTSPOTS = [
+        (8390, True),
+        (8566, False),
+        (8701, True),
+        (8860, True),
+]
 
 @pytest.mark.workflow('test-snv-indels-chrM')
-def test_is_in_hotspot2(workflow_dir):
+@pytest.mark.parametrize(["pos", "hotspot"], HOTSPOTS)
+def test_is_in_hotspot2(workflow_dir, pos, hotspot):
     sample = "SRR8615409"
     output_file = pathlib.Path(workflow_dir, f"{sample}/snv-indels/{sample}.vep.high.txt")
 
-    # Expected results, there should be 4 variants in the vep.high.txt file
-    positions = [8390, 8566, 8701, 8860]
-    is_in_hotspot = [True, False, True, True]
-
+    # Read the file, and pick out the line we want based on pos
     with open(output_file) as fin:
-        for pos, hotspot, line in zip(positions, is_in_hotspot, fin):
+        for line in fin:
             d = json.loads(line)
-            assert d["start"] == pos
-            assert d["is_in_hotspot"] == hotspot
+            if d["start"] == pos:
+                assert d["is_in_hotspot"] == hotspot
+                break
+        else:
+            raise RuntimeError(f"Position {pos} not found in {output_file}")
