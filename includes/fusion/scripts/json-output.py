eceaafce572977165f82ec51a8d3ab4e8f5a847c
Redmar van den Berg <RedmarvandenBerg@lumc.nl>
2023-08-22 07:33:29 +0200

    Add fusion plots to json output

--- includes/fusion/scripts/json-output.py
+++ includes/fusion/scripts/json-output.py
@@ -5,20 +5,29 @@ import argparse
 import os
 
 
-def fusion_results(args):
+def fusion_results(fname):
     # Initialise the results dictionary
-    with open(args.arriba) as fin:
+    with open(fname) as fin:
         return json.load(fin)
 
 def main(args):
     """ Create json output of fusion results """
-    results = fusion_results(args)
+    results = fusion_results(args.arriba)
+    for i, fusion in enumerate(results, 1):
+        # Here, we magically know the name of the png file.
+        plot = os.path.join(args.plots, f"fusion-{i}.png")
+        if not os.path.exists(plot):
+            raise RuntimeError(f"Missing fusion figure: {plot}")
+
+        fusion["plot"] = plot
+
     print(json.dumps({"fusion": results}, sort_keys=True, indent=2))
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument('arriba', help='Arriba output converted to JSON')
+    parser.add_argument('plots', help='Arriba fusion plots')
 
     args = parser.parse_args()
     main(args)
