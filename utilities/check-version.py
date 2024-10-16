dc3f408d4429cd3324872c4fb6bf0370a9e0a699
Redmar van den Berg <RedmarvandenBerg@lumc.nl>
2024-10-16 11:04:03 +0200

    Check version in the documentation

--- utilities/check-version.py
+++ utilities/check-version.py
@@ -22,17 +22,30 @@ def get_hamlet_version() -> str:
                 return hamlet_version
     return ""
 
+
+def get_doc_version() -> str:
+    config = "docs/source/conf.py"
+    with open(config) as fin:
+        for line in fin:
+            if line.startswith("release = "):
+                doc_version = line.split('"')[1]
+                return doc_version
+    return ""
+
+
 if __name__ == "__main__":
 
     version: str = sys.argv[1]
 
     changelog_version = get_changelog_version()
     hamlet_version = get_hamlet_version()
+    docs_version = get_doc_version()
     print(f"Expected version:  {version}")
     print(f"Changelog version: {changelog_version}")
     print(f"HAMLET version:    {hamlet_version}")
+    print(f"Docs version:      {docs_version}")
 
-    if changelog_version == version and hamlet_version == version:
+    if changelog_version == version and hamlet_version == version and docs_version == version:
         exit(0)
     else:
         exit(1)
