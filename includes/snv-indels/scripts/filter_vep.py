6369175dc9a9288be403ef062ac08374a30320b9
Redmar van den Berg <RedmarvandenBerg@lumc.nl>
2025-05-08 14:09:24 +0200

    Refactor filter code into a single file

--- includes/snv-indels/scripts/filter_vep.py
+++ includes/snv-indels/scripts/filter_vep.py
@@ -1,64 +1,33 @@
 #!/usr/bin/env python3
 
+# Authors: Anne van der Grinten, Redmar van den Berg
+
 import argparse
 import json
 from itertools import zip_longest
 
+from mutalyzer_hgvs_parser import to_model  # type: ignore
+from collections import namedtuple
+from collections.abc import Iterator
+import functools
 
+from typing import Any, Dict, Set, Tuple
 
-from filter_variants import Variant, Criterion
-from typing import Any, Dict, Set, Iterator
 
 # Type for the frequencies entry from VEP
 FrequenciesType = Dict[str, Dict[str, float]]
-# From most to least severe, taken from the ensembl website
-# https://www.ensembl.org/info/genome/variation/prediction/predicted_data.html
-severity = [
-    "transcript_ablation",
-    "splice_acceptor_variant",
-    "splice_donor_variant",
-    "stop_gained",
-    "frameshift_variant",
-    "stop_lost",
-    "start_lost",
-    "transcript_amplification",
-    "inframe_insertion",
-    "inframe_deletion",
-    "missense_variant",
-    "protein_altering_variant",
-    "splice_region_variant",
-    "splice_donor_5th_base_variant",
-    "splice_donor_region_variant",
-    "splice_polypyrimidine_tract_variant",
-    "incomplete_terminal_codon_variant",
-    "start_retained_variant",
-    "stop_retained_variant",
-    "synonymous_variant",
-    "coding_sequence_variant",
-    "mature_miRNA_variant",
-    "5_prime_UTR_variant",
-    "3_prime_UTR_variant",
-    "non_coding_transcript_exon_variant",
-    "intron_variant",
-    "NMD_transcript_variant",
-    "non_coding_transcript_variant",
-    "upstream_gene_variant",
-    "downstream_gene_variant",
-    "TFBS_ablation",
-    "TFBS_amplification",
-    "TF_binding_site_variant",
-    "regulatory_region_ablation",
-    "regulatory_region_amplification",
-    "feature_elongation",
-    "regulatory_region_variant",
-    "feature_truncation",
-    "intergenic_variant",
-]
+
+# Tuple to store locations of HGVS points
+Location = namedtuple("Location", ["downstream", "position", "offset"])
+
+# Tuple to store a region
+Region = namedtuple("Region", ["start", "end"])
+
 
 class VEP(dict[str, Any]):
     """Class to work with VEP objects"""
 
-    def filter_criteria(self, criteria: list[Criterion]) -> None:
+    def filter_criteria(self, criteria: list["Criterion"]) -> None:
         filtered = list()
         for tc in self.get("transcript_consequences", list()):
             hgvsc = tc.get("hgvsc")
@@ -168,6 +137,243 @@ class VEP(dict[str, Any]):
         return f"{chrom}:{pos}"
 
 
+class Variant:
+    def __init__(self, hgvs: str, consequences: list[str]):
+        self.hgvs = hgvs
+        self.consequences = consequences
+
+    @classmethod
+    def from_VEP(cls, vep: VEP) -> Iterator["Variant"]:
+        """Yield all Variants from a VEP record"""
+        for transcript in vep.get("transcript_consequences", list()):
+            consequences = transcript["consequence_terms"]
+            for hgvs in ["hgvsg", "hgvsc", "hgvsp"]:
+                if hgvs in transcript:
+                    yield Variant(transcript[hgvs], consequences)
+
+    def __str__(self) -> str:
+        return f"Variant({self.hgvs}, {self.consequences})"
+
+    def __repr__(self) -> str:
+        return str(self)
+
+    def __eq__(self, other: object) -> bool:
+        if not isinstance(other, Variant):
+            raise NotImplementedError
+        return str(self) == str(other)
+
+
+class Criterion:
+    def __init__(
+        self,
+        identifier: str,
+        coordinate: str = "c",
+        consequence: str | None = None,
+        start: str | None = None,
+        end: str | None = None,
+    ) -> None:
+        self.identifier = identifier
+        self.coordinate = coordinate
+        self.consequence = consequence
+
+        # Define the type of start and end
+        self.start: Location | None
+        self.end: Location | None
+
+        if start is not None:
+            # if end is not specified, set end equal to start
+            if end is None:
+                end = start
+            # Make a fake hgvs description to determine the start and end Location
+            hgvs = f"{identifier}:c.{start}_{end}"
+            self.start, self.end = get_position(hgvs)
+        else:
+            self.start = None
+            self.end = None
+
+    def __str__(self) -> str:
+        return (
+            f"Criterion(identifier={self.identifier}, "
+            f"coordinate={self.coordinate}, "
+            f"consequence={self.consequence}, "
+            f"start={self.start}, "
+            f"end={self.end})"
+        )
+
+    def __repr__(self) -> str:
+        return str(self)
+
+    def match(self, variant: Variant) -> bool:
+        return all(
+            [
+                self.match_id(variant),
+                self.match_coordinate(variant),
+                self.match_consequence(variant),
+                self.match_region(variant),
+            ]
+        )
+
+    def match_id(self, variant: Variant) -> bool:
+        """Determine if the identifier matches the variant
+
+        Raises a runtime error if only the version doesn't match, since this
+        could indicate the use of incompatible reference seqeunces
+        """
+
+        def split_version(variant_id: str) -> Tuple[str, str]:
+            """Split the version from the identifier
+
+            Set version to 0 if there is no version
+            """
+            try:
+                id_, version = variant_id.split(".")
+            # If there is no version (e.g. chr5), set version to 0
+            except ValueError:
+                id_ = variant_id
+                version = "0"
+
+            return id_, version
+
+        # Get the variant id
+        variant_id = variant.hgvs.split(":")[0]
+        criterion_id = self.identifier
+
+        id1, v1 = split_version(variant_id)
+        id2, v2 = split_version(criterion_id)
+
+        if id1 != id2:
+            return False
+
+        if v1 == v2:
+            return True
+        else:
+            msg = f"Version mismatch between {variant} and {self}"
+            raise RuntimeError(msg)
+
+    def match_coordinate(self, variant: Variant) -> bool:
+        variant_coordinate = variant.hgvs.split(":")[1].split(".")[0]
+        return variant_coordinate == self.coordinate
+
+    def match_consequence(self, variant: Variant) -> bool:
+        if self.consequence is None:
+            return True
+        return self.consequence in variant.consequences
+
+    def match_region(self, variant: Variant) -> bool:
+        if self.start is None:
+            return True
+        var_region = get_position(variant.hgvs)
+        crit_region = Region(self.start, self.end)
+
+        return region_overlap(var_region, crit_region)
+
+
+@functools.lru_cache(maxsize=1000)
+def get_position(hgvs: str) -> Region:
+    # Get the variant part of the hgvs description
+    coordinate, var = hgvs.split(":")[1].split(".")
+
+    if coordinate == "p":
+        model = to_model(var, "p_variant")
+    else:
+        model = to_model(var, "variant")
+
+    location = model["location"]
+    if location["type"] == "point":
+        start = point_to_tuple(location)
+        end = start
+    elif location["type"] == "range":
+        start = point_to_tuple(location["start"])
+        end = point_to_tuple(location["end"])
+
+        # make sure "end" is downstream of "start"
+        if start > end:
+            s, e = start, end
+            start = e
+            end = s
+    else:
+        raise RuntimeError
+
+    return Region(start, end)
+
+
+def point_to_tuple(point: dict[str, Any]) -> Location:
+    """Convert a 'point' dictionary from parse_hgvs into a Location"""
+
+    # Default values
+    downstream = 0
+    position = 0
+    offset = 0
+
+    position = point["position"]
+    if "outside_cds" in point:
+        if point["outside_cds"] == "upstream":
+            position *= -1
+        elif point["outside_cds"] == "downstream":
+            downstream = 1
+        else:
+            raise RuntimeError
+    if "offset" in point:
+        offset = point["offset"]["value"]
+
+    return Location(downstream, position, offset)
+
+
+def region_overlap(region1: Region, region2: Region) -> bool:
+    return any(
+        [
+            region1[0] >= region2[0] and region1[0] <= region2[1],
+            region1[1] >= region2[0] and region1[1] <= region2[1],
+            region1[0] < region2[0] and region1[1] > region2[1],
+        ]
+    )
+
+
+# From most to least severe, taken from the ensembl website
+# https://www.ensembl.org/info/genome/variation/prediction/predicted_data.html
+severity = [
+    "transcript_ablation",
+    "splice_acceptor_variant",
+    "splice_donor_variant",
+    "stop_gained",
+    "frameshift_variant",
+    "stop_lost",
+    "start_lost",
+    "transcript_amplification",
+    "inframe_insertion",
+    "inframe_deletion",
+    "missense_variant",
+    "protein_altering_variant",
+    "splice_region_variant",
+    "splice_donor_5th_base_variant",
+    "splice_donor_region_variant",
+    "splice_polypyrimidine_tract_variant",
+    "incomplete_terminal_codon_variant",
+    "start_retained_variant",
+    "stop_retained_variant",
+    "synonymous_variant",
+    "coding_sequence_variant",
+    "mature_miRNA_variant",
+    "5_prime_UTR_variant",
+    "3_prime_UTR_variant",
+    "non_coding_transcript_exon_variant",
+    "intron_variant",
+    "NMD_transcript_variant",
+    "non_coding_transcript_variant",
+    "upstream_gene_variant",
+    "downstream_gene_variant",
+    "TFBS_ablation",
+    "TFBS_amplification",
+    "TF_binding_site_variant",
+    "regulatory_region_ablation",
+    "regulatory_region_amplification",
+    "feature_elongation",
+    "regulatory_region_variant",
+    "feature_truncation",
+    "intergenic_variant",
+]
+
+
 def read_criteria_file(criteria_file):
     """Read the criterions file"""
     criteria = list()
@@ -175,7 +381,7 @@ def read_criteria_file(criteria_file):
     header = None
     with open(criteria_file) as fin:
         for line in fin:
-            if line.startswith('#'):
+            if line.startswith("#"):
                 continue
 
             spline = line.strip("\n").split("\t")
@@ -184,7 +390,7 @@ def read_criteria_file(criteria_file):
                 continue
 
             # Read into dict, convert '' to None
-            d = {k:v if v else None for k,v in zip_longest(header,spline)}
+            d = {k: v if v else None for k, v in zip_longest(header, spline)}
 
             # Check that at least the transcript id is set
             transcript_id = d.get("transcript_id")
@@ -195,7 +401,7 @@ def read_criteria_file(criteria_file):
                 coordinate="c",
                 consequence=d["consequence"],
                 start=d["start"],
-                end=d["end"]
+                end=d["end"],
             )
 
             criteria.append(c)
--- includes/snv-indels/scripts/test_filter_vep.py
+++ includes/snv-indels/scripts/test_filter_vep.py
@@ -1,12 +1,23 @@
 #!/usr/bin/env python3
 
-from filter_vep import VEP, FrequenciesType
-from filter_variants import Criterion
+# Authors: Anne van der Grinten, Redmar van den Berg
 
 import json
 import pytest
 
-from typing import Any, Dict, Set, List, Tuple
+from filter_vep import (
+    VEP,
+    FrequenciesType,
+    Criterion,
+    Variant,
+    Location,
+    Region,
+    region_overlap,
+    get_position,
+)
+
+
+from typing import Any, Dict, List, Tuple
 
 
 @pytest.fixture
@@ -16,19 +27,21 @@ def vep() -> VEP:
     return VEP(js[0])
 
 
-CRITERIA= [
-        # Criteria, readout of gene names
-        ([Criterion("ENTS0123.1")], ["gene1"]),
-        ([Criterion("ENTS0123.1"), Criterion("ENTS0124.1")], ["gene1", "gene2"]),
-        ([Criterion("ENST999.9")], []),
-        ([Criterion("ENTS0123.1", consequence="transcript_ablation")], ["gene1"]),
-        ([Criterion("ENTS0125.1", consequence="stop_gained")], ["gene3"]),
-        ([Criterion("ENTS0125.1", consequence="inframe_insertion")], ["gene3"]),
+CRITERIA = [
+    # Criteria, readout of gene names
+    ([Criterion("ENTS0123.1")], ["gene1"]),
+    ([Criterion("ENTS0123.1"), Criterion("ENTS0124.1")], ["gene1", "gene2"]),
+    ([Criterion("ENST999.9")], []),
+    ([Criterion("ENTS0123.1", consequence="transcript_ablation")], ["gene1"]),
+    ([Criterion("ENTS0125.1", consequence="stop_gained")], ["gene3"]),
+    ([Criterion("ENTS0125.1", consequence="inframe_insertion")], ["gene3"]),
 ]
 
+
 @pytest.mark.parametrize(["criteria", "readout"], CRITERIA)
-def test_filter_criteria(vep: VEP, criteria: list[Criterion],
-                                 readout: list[str]) -> None:
+def test_filter_criteria(
+    vep: VEP, criteria: list[Criterion], readout: list[str]
+) -> None:
     """Test filtering by consequence_term"""
     assert len(vep["transcript_consequences"]) == 3
     vep.filter_criteria(criteria)
@@ -63,7 +76,7 @@ def test_vep_of_interest_one_transcript(vep: VEP) -> None:
     assert len(cons) == 1
     assert cons[0]["gene_id"] == "gene3"
     assert cons[0]["transcript_id"] == "transcript3"
-    assert cons[0]["consequence_terms"] == ["inframe_insertion","stop_gained"]
+    assert cons[0]["consequence_terms"] == ["inframe_insertion", "stop_gained"]
 
     # Test that we set the most severe consequence for transcript3
     assert vep["most_severe_consequence"] == "stop_gained"
@@ -93,6 +106,7 @@ def test_no_transcript_consequence() -> None:
     empty.update_most_severe()
 
 
+# fmt: off
 COLOCATED_VARIANTS :List[Tuple[Any,Any]]= [
     # No "colocated_variants" entry
     (dict(), dict()),
@@ -148,6 +162,8 @@ COLOCATED_VARIANTS :List[Tuple[Any,Any]]= [
         {"T": {"af": 0.5}}
     ),
 ]
+# fmt: on
+
 
 @pytest.mark.parametrize(["vep", "frequencies"], COLOCATED_VARIANTS)
 def test_extract_frequencies(vep: VEP, frequencies: FrequenciesType) -> None:
@@ -161,6 +177,7 @@ def test_error_extract_multiple_frequencies() -> None:
     WHEN we extract the frequency values
     THEN we get an error
     """
+    # fmt: off
     data: Dict[str, Any] = {
             "colocated_variants": [
                 {
@@ -171,9 +188,11 @@ def test_error_extract_multiple_frequencies() -> None:
                 }
             ]
     }
+    # fmt: on
     V = VEP(data)
     with pytest.raises(RuntimeError):
         V._extract_frequencies()
+
 
 def test_error_extract_frequencies_multiple_entries() -> None:
     """
@@ -181,6 +200,7 @@ def test_error_extract_frequencies_multiple_entries() -> None:
     WHEN we extract the frequency values
     THEN we get an error
     """
+    # fmt: off
     data: Dict[str, Any] = {"colocated_variants": [
             {"frequencies":
                 {
@@ -190,18 +210,22 @@ def test_error_extract_frequencies_multiple_entries() -> None:
             }
         ]
     }
+    # fmt: on
     V = VEP(data)
     with pytest.raises(RuntimeError):
         V._extract_frequencies()
 
+
 POPULATION = [
-        # If frequencies is empty, return 0
-        ({}, 0),
-        # If the specified population is missing, return 0
-        ({"T": {}}, 0),
-        # Otherwise, return the requested population frequency
-        ({"T": {"gnomAD": 0.5}}, 0.5),
+    # If frequencies is empty, return 0
+    ({}, 0),
+    # If the specified population is missing, return 0
+    ({"T": {}}, 0),
+    # Otherwise, return the requested population frequency
+    ({"T": {"gnomAD": 0.5}}, 0.5),
 ]
+
+
 @pytest.mark.parametrize(["frequencies", "expected"], POPULATION)
 def test_extract_population(frequencies: FrequenciesType, expected: float) -> None:
     """
@@ -223,6 +247,7 @@ def test_extract_population_frequency() -> None:
         VEP_.extract_frequencies
         VEP._extract_population
     """
+    # fmt: off
     data = {
         "colocated_variants": [
             # An empty colocated variant
@@ -239,6 +264,7 @@ def test_extract_population_frequency() -> None:
             }
         ]
     }
+    # fmt: on
 
     V = VEP(data)
     assert V.population_frequency("gnomAD") == 0.42
@@ -252,8 +278,13 @@ THRESHOLD = [
     # nothing is above the threshold
     ("Missing", 1, False),
 ]
+
+
 @pytest.mark.parametrize(["population", "threshold", "expected"], THRESHOLD)
-def test_above_population_threshold(population: str, threshold:float, expected: bool) -> None:
+def test_above_population_threshold(
+    population: str, threshold: float, expected: bool
+) -> None:
+    # fmt: off
     data = {
         "colocated_variants": [
             # An empty colocated variant
@@ -270,6 +301,366 @@ def test_above_population_threshold(population: str, threshold:float, expected: 
             }
         ]
     }
-    V =VEP(data)
+    # fmt: on
+    V = VEP(data)
 
     assert V.above_population_threshold(population, threshold) is expected
+
+
+@pytest.fixture
+def variant() -> Variant:
+    return Variant("ENST123.5:c.10A>T", consequences=["missense", "inframe"])
+
+
+@pytest.fixture
+def minimal_vep() -> VEP:
+    # fmt: off
+    minimal_vep_record = {
+        "transcript_consequences": [
+            {
+                "consequence_terms": ["inframe_insertion"],
+                "hgvsg": "chr13:g.28034118_28034147dup",
+                "hgvsc": "ENST00000241453.1:c.1772_1801dup",
+                "hgvsp": "ENSP00000241453.1:p.Asp600_Leu601insHisValAspPheArgGluTyrGluTyrAsp",
+            },
+            {
+                "consequence_terms": ["inframe_insertion", "NMD_transcript_variant"],
+                "hgvsg": "chr13:g.28034118_28034147dup",
+                "hgvsc": "ENST00000380987.1:c.1772_1801dup",
+                "hgvsp": "ENSP00000370374.1:p.Asp600_Leu601insHisValAspPheArgGluTyrGluTyrAsp",
+            },
+        ]
+    }
+    # fmt: on
+
+    return minimal_vep_record
+
+
+def test_create_criterion() -> None:
+    C = Criterion("ENST123.5")
+    assert C.identifier == "ENST123.5"
+
+
+def test_create_criterion_Locations() -> None:
+    """
+    WHEN we initialize a criterion
+    THEN start and end must be Location tuples
+    """
+    C = Criterion("ENST123.5", start="10", end="*20-5")
+
+    assert C.start == Location(0, 10, 0)
+    assert C.end == Location(1, 20, -5)
+
+
+def test_create_variant(variant: Variant) -> None:
+    assert variant.hgvs == "ENST123.5:c.10A>T"
+    assert variant.consequences == ["missense", "inframe"]
+
+
+def test_variant_match(variant: Variant) -> None:
+    """not
+    GIVEN a variant and a criterion with different transcripts
+    WHEN we check for a match
+    THEN we should not find a match
+    """
+    C = Criterion("ENST120.5")
+
+    assert not C.match(variant)
+
+
+def test_transcript_match(variant: Variant) -> None:
+    """
+    GIVEN two identical transcripts
+    WHEN we check for a match
+    THEN we should find a match
+    """
+    C = Criterion("ENST123.5")
+    assert C.match_id(variant)
+
+
+def test_transcript_no_match(variant: Variant) -> None:
+    """
+    GIVEN two different transcripts
+    WHEN we check for a match
+    THEN we should not find a match
+    """
+    C = Criterion("ENST120.5")
+    assert not C.match_id(variant)
+
+
+def test_transcript_version_no_match(variant: Variant) -> None:
+    """
+    GIVEN a version mismatch between two transcripts
+    WHEN we check for a match
+    THEN we should get a runtime error
+    """
+    C = Criterion("ENST123.4")
+    with pytest.raises(RuntimeError):
+        C.match_id(variant)
+
+
+def test_transcript_id_no_version(variant: Variant) -> None:
+    """
+    GIVEN a Transcript and Criterion without version number
+    WHEN we compare them
+    THEN they should match
+    """
+    V = Variant("Chr12:g.10A>T", consequences=[])
+    C = Criterion("Chr12", coordinate="g")
+
+    assert C.match_id(V)
+
+
+def test_transcript_version_no_match_error(variant: Variant) -> None:
+    """
+    GIVEN a version mismatch between criterion and variant
+    WHEN we check for a match
+    THEN we should get a runtime error
+    """
+    C = Criterion("ENST123.4")
+    with pytest.raises(RuntimeError):
+        C.match(variant)
+
+
+CONS = [(None, True), ("missense", True), ("stop lost", False)]
+
+
+@pytest.mark.parametrize("consequence, expected", CONS)
+def test_match_consequence(
+    consequence: str | None, expected: bool, variant: Variant
+) -> None:
+    """
+    GIVEN a variant and a criterion with identical transcript and a consequence
+    WHEN we check for a match
+    THEN we should get the answer that is expected
+    """
+    C = Criterion("ENST123.5", "c", consequence)
+    assert C.match(variant) == expected
+
+
+# fmt: off
+POSITION = [
+    ("-20del",(
+        Location(0,-20,0),
+        Location(0,-20,0)
+    )),
+    ("-10+5del",(
+        Location(0,-10,5),
+        Location(0,-10,5)
+    )),
+    ("-9-5del",(
+        Location(0,-9,-5),
+        Location(0,-9,-5)
+    )),
+    ("1del",(
+        Location(0,1,0),
+        Location(0,1,0)
+    )),
+    ("10+5del",(
+        Location(0,10,+5),
+        Location(0,10,+5)
+    )),
+    ("11-5del",(
+        Location(0,11,-5),
+        Location(0,11,-5)
+    )),
+    ("*1del",(
+        Location(1,1,0),
+        Location(1,1,0)
+    )),
+    ("*5+5del",(
+        Location(1,5,5),
+        Location(1,5,5)
+    )),
+    ("*6-5del",(
+        Location(1,6,-5),
+        Location(1,6,-5)
+    )),
+    ("10_15del",(
+        Location(0,10,0),
+        Location(0,15,0)
+    )),
+    #in case start and end are specified antisense in criterion
+    ("15_10del",(
+        Location(0,10,0),
+        Location(0,15,0)
+    )),
+]
+# fmt: on
+
+
+@pytest.mark.parametrize("mutation, expected_position", POSITION)
+def test_variant_position_calculation(
+    mutation: str, expected_position: tuple[Location, Location]
+) -> None:
+    """
+    GIVEN a mutation
+    WHEN we extract the position from hgvs
+    THEN we should get a 3-integer tuple for start and end
+    """
+    hgvs = "ENST123.5:c." + mutation
+    V1 = Variant(hgvs, consequences=["missense"])
+    assert get_position(V1.hgvs) == expected_position
+
+
+def test_variant_position_no_change_protein() -> None:
+    """
+    GIVEN a protein variant with no amino acid change
+    WHEN we extract the position from the HGVS
+    THEN we should not get an error
+    """
+    hgvs = "ENSP123.5:p.Leu615="
+    assert get_position(hgvs) == (Location(0, 615, 0), Location(0, 615, 0))
+
+
+# fmt: off
+POSITION_CRITERION = [
+    ("10","15",(
+        Location(0,10,0),
+        Location(0,15,0)
+    )),
+    ("15","10",(
+        Location(0,10,0),
+        Location(0,15,0)
+    )),
+    ("15",None,(
+        Location(0,15,0),
+        Location(0,15,0)
+    ))
+]
+# fmt: on
+
+
+@pytest.mark.parametrize("start,end,expected_position_crit", POSITION_CRITERION)
+def test_criterion_position_calculation(
+    start: str, end: str, expected_position_crit: tuple[Location, Location]
+) -> None:
+    """
+    GIVEN a criterion
+    WHEN we convert start and end into positions
+    THEN we should get a 3-integer tuple for start and end
+    """
+    C1 = Criterion("ENST123.5", start=start, end=end)
+    assert (C1.start, C1.end) == expected_position_crit
+
+
+# fmt: off
+REGION = [
+    # region1 lies before region2
+    ((1,2),(5,9),False),
+    # r1 ends on start r2
+    ((3,5),(5,9),True),
+    # r1 ends inside r2
+    ((3,6),(5,9),True),
+    # r1 ends on end r2
+    ((3,9),(5,9),True),
+    # r2 is inside r1
+    ((3,10),(5,9),True),
+    # r1 starts on start r2, ends inside r2
+    ((5,6),(5,9),True),
+    # r1 starts on start r2, ends on end r2
+    ((5,9),(5,9),True),
+    # r1 starts on start r2, ends outside r2
+    ((5,10),(5,9),True),
+    # r1 starts and ends inside r2
+    ((6,7),(5,9),True),
+    # r1 starts in r2, ends on end r2
+    ((6,9),(5,9),True),
+    # r1 starts in r2, ends outside r2
+    ((6,10),(5,9),True),
+    # r1 starts at end r2, ends outside r2
+    ((9,10),(5,9),True),
+    # r1 lies after r2
+    ((10,11),(5,9),False)
+]
+# fmt: on
+
+
+@pytest.mark.parametrize("region1, region2, overlap", REGION)
+def test_region_overlap(region1: Region, region2: Region, overlap: bool) -> None:
+    """
+    GIVEN two regions
+    WHEN we call the overlap
+    THEN we should get whether the regions overlap
+
+    Important: assumes inclusive positions, so (1,5) and (5, 6) overlap
+    """
+
+    assert region_overlap(region1, region2) == overlap
+
+
+@pytest.mark.parametrize("region1, region2, overlap", REGION)
+def test_region_overlap_reverse(
+    region1: Region, region2: Region, overlap: bool
+) -> None:
+    """
+    GIVEN two regions
+    WHEN we call the overlap
+    THEN we should get whether the regions overlap
+
+    Important: assumes inclusive positions, so (1,5) and (5, 6) overlap
+    """
+
+    assert region_overlap(region2, region1) == overlap
+
+
+def test_match_region(variant: Variant) -> None:
+    """
+    GIVEN a criterion and a variant in the specified region
+    WHEN we check the match
+    THEN we should see that the variant matches the criterion
+    """
+    c = Criterion("ENST123.5", consequence="inframe", start="10", end="20")
+    assert c.match(variant) == True
+
+
+def test_match_coordinate(variant: Variant) -> None:
+    """
+    GIVEN a Variant and Criterion
+    WHEN we test if the coordinates match
+    THEN they should only match when they are identical
+    """
+    c = Criterion("ENST123.5", coordinate="c")
+    g = Criterion("ENST123.5", coordinate="g")
+
+    assert c.match_coordinate(variant)
+    assert not g.match_coordinate(variant)
+
+
+def test_criterion_coordinate_mismatch(variant: Variant) -> None:
+    """
+    GIVEN a Variant and Criterion where only the coordinates don't match
+    WHEN we match them
+    THEN the Criterion should not match
+    """
+    c = Criterion("ENST123.5", coordinate="r", consequence="missense")
+    assert not c.match(variant)
+
+
+def test_Variant_from_VEP(minimal_vep: VEP) -> None:
+    """
+    GIVEN a VEP record
+    WHEN we generate Variants from the VEP record
+    THEN we should get a Variant for every hgvs description in every transcript_consequences
+    """
+    expected = [
+        Variant("chr13:g.28034118_28034147dup", ["inframe_insertion"]),
+        Variant("ENST00000241453.1:c.1772_1801dup", ["inframe_insertion"]),
+        Variant(
+            "ENSP00000241453.1:p.Asp600_Leu601insHisValAspPheArgGluTyrGluTyrAsp",
+            ["inframe_insertion"],
+        ),
+        Variant(
+            "chr13:g.28034118_28034147dup",
+            ["inframe_insertion", "NMD_transcript_variant"],
+        ),
+        Variant(
+            "ENST00000380987.1:c.1772_1801dup",
+            ["inframe_insertion", "NMD_transcript_variant"],
+        ),
+        Variant(
+            "ENSP00000370374.1:p.Asp600_Leu601insHisValAspPheArgGluTyrGluTyrAsp",
+            ["inframe_insertion", "NMD_transcript_variant"],
+        ),
+    ]
+    assert list(Variant.from_VEP(minimal_vep)) == expected
