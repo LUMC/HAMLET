a86588b1f79ee043c8f0b10a3839313e9e6c3f0c
Redmar van den Berg <RedmarvandenBerg@lumc.nl>
2025-09-24 10:42:57 +0200

    Add optional input file for known variants

--- includes/snv-indels/scripts/annotate_vep.py
+++ includes/snv-indels/scripts/annotate_vep.py
@@ -49,15 +49,25 @@ def parse_vep_json(vep_file: str) -> Iterator[VEP]:
         for line in fin:
             yield VEP(json.loads(line))
 
-def main(vep_file: str, annotations_file: str):
+def read_known_variants(fname: str) -> dict[str, str]:
+    raise NotImplementedError
+
+def main(vep_file: str, annotations_file: str, known_variants_file: str | None):
     criteria = read_criteria_file(annotations_file)
+    known_variants = read_known_variants(known_variants_file) if known_variants_file else dict()
 
     for vep in parse_vep_json(vep_file):
         for transcript in vep["transcript_consequences"]:
             # Make sure hgvsc is defined
             hgvsc = transcript.get("hgvsc")
+            # If there is no HGVSC, we do nothing
             if hgvsc is None:
                 continue
+            # If hgvsc is a known variant, we update the annotation
+            if hgvsc in known_variants:
+                transcript["annotation"] = known_variants[hgvsc]
+                continue
+            # Otherwise, we check the criteria
             variant = Variant(hgvsc, transcript["consequence_terms"])
             for crit, annotation in criteria.items():
                 if crit.match(variant):
@@ -74,10 +84,12 @@ if __name__ == "__main__":
 
     parser.add_argument("--vep", help="VEP json output file", required=True)
     parser.add_argument("--annotations", help="Criteria file with annotations", required=True)
+    parser.add_argument("--known-variants", help="File with known pathogenic variants", nargs='?')
 
     args = parser.parse_args()
 
     main(
         args.vep,
-        args.annotations
+        args.annotations,
+        args.known_variants
     )
